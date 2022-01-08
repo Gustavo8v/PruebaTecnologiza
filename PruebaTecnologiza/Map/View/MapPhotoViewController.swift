724790a4e281b2cbb175fea4e3946eedfdfc3391
Tavo <oliver.sykes.van@icloud.com>
2022-01-07 23:08:01 -0600

    pin location User

--- PruebaTecnologiza/Map/View/MapPhotoViewController.swift
+++ PruebaTecnologiza/Map/View/MapPhotoViewController.swift
@@ -13,6 +13,7 @@ class MapPhotoViewController: UIViewController {
     @IBOutlet weak var mapPhotographUser: MKMapView!
     
     var coordinateRegion = MKCoordinateRegion()
+    var pinMap = MKPointAnnotation()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,14 +22,18 @@ class MapPhotoViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         mapPhotographUser.setRegion(coordinateRegion, animated: true)
+        mapPhotographUser.addAnnotation(pinMap)
     }
     
     func renderMap(_ location: CLLocation) {
         let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude,
                                                 longitude: location.coordinate.longitude)
-        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
         let region = MKCoordinateRegion(center: coordinate,
                                         span: span)
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinate
+        pinMap = pin
         coordinateRegion = region
     }
 }
